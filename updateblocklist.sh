25c29adf79509bbd05f610b746557ac370e2f3e5
JimMatthew <jameslindstrom2@gmail.com>
2023-12-13 08:08:55 -0600

    Update updateblocklist.sh

--- updateblocklist.sh
+++ updateblocklist.sh
@@ -1,1 +1,16 @@
+#!/bin/sh
 
+BLOCKLISTS="
+https://small.oisd.nl/unbound
+"
+BLOCKLIST_FILE='/etc/unbound/blacklist.conf'
+
+echo updating blocklist...
+echo '' > $BLOCKLIST_FILE
+
+for p in $BLOCKLISTS; do
+        curl -s $p >> $BLOCKLIST_FILE
+done
+
+unbound-control reload
+unbound-control start
